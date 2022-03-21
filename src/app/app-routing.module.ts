c8a4b4ebd3a0567fe502266ada6d89d2f861ca27
Gonzalo <gonga.silva1999@gmail.com>
2022-03-21 17:09:45 -0300

    Init v2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { AuthGuard } from './core/guards/auth.guard';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'auth',
     pathMatch: 'full',
   },
   {
