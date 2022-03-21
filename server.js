762dd4d039b4218062f200d142aeaa95d9ff06ec
Gonzalo <gonga.silva1999@gmail.com>
2022-03-21 16:56:16 -0300

    Init v2

--- server.js
+++ server.js
@@ -1,18 +1,15 @@
-function requireHTTPS(req, res, next) {
-    // The 'x-forwarded-p roto' check is for Heroku
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-        return res.redirect('https://' + req.get('host') + req.url);
-    }
-    next();
-}
+//Install express server
 const express = require('express');
+const path = require('path');
+
 const app = express();
-app.use(requireHTTPS);
-app.use(express.static("./dist/talkingmarket-front.json"));
 
-app.get("/*", function(req, res) {
-    res.sendFile("index.html", {root: "dist/talkingmarket-front.json"}
-  );
-});
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/angular-app-heroku'));
 
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/angular-app-heroku/'}),
+);
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
