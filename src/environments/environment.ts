d1c9167905d4449f466b24c42b47bea120460578
Gonzalo <gonga.silva1999@gmail.com>
2022-03-21 14:48:12 -0300

    Init

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   apiHost: 'https://talking-market-back.herokuapp.com',
 };
 
